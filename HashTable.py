72e0eccfb34f6f2cfa0274013746c636a342bf5d
jvichn1 <jvichn1@wgu.edu>
2025-08-22 15:09:51 -0400

    HashTable methods complete

--- HashTable.py
+++ HashTable.py
@@ -2,7 +2,8 @@
 class HashTable:
     
     # Set size to 40 because that's how many packages we have in the project
-    def __init__(self, size=40):
+    # thoughts on making this size dynamic later?
+    def __init__(self, size = 40):
 
         # Initialize the hash table with a specified size
         self.size = size
@@ -15,52 +16,62 @@ class HashTable:
         """Private method to compute the hash value for a given key."""
 
         # modulo to map a key (like package_id) to a bucket index
-        return key % self.size
+        return key % len(self.size)
+
 
     # insert method to add a key-value pair to the hash table
-    # - use _hash to find the bucket index
-    # - check if the key already exists in the bucket
-    # - else, append the key-value pair to the bucket
-
     def insert(self, key, value):
-
+       
+        # - check if the key already exists in the bucket
         """Add a key-value pair to the hashmap."""
         if key in self.data:
 
-            raise KeyError(f"Key '{key}' already exists.")
+            # if it exists, update the value
+            self.data[key] = value
 
-        self.data[key] = value
+        # - else, append the key-value pair to the bucket
+        else:
 
-        # redundant, please decide!!
-    def insert(self, key, value):
-        """Insert a key-value pair into the hash table."""
-        index = self._hash(key)
-        bucket = self.table[index]
-        # Check if the key already exists in the bucket
-        for i, (k, v) in enumerate(bucket):
-            if k == key:
-                # If it exists, update the value
-                bucket[i] = (key, value)
-                return
-    # 
-    # 
-    # 
-    # 
-    # TODO: Write a get(self, key) method to retrieve a value by its key
-    # use _hash to find the bucket index
-    # search the bucket for the key
-    # return the value if found, else return None
-    # 
+            # - if it does not exist, append the key-value pair to the bucket
+            index = self._hash(key)
+            bucket = self.table[index]
+
+            # check if the key already exists in the bucket
+            for i, (k, v) in enumerate(bucket):
+                if k == key:
+
+                    # if it exists, update the value
+                    bucket[i] = (key, value)
+                    return
+
+            # if the key does not exist, append the new key-value pair
+            bucket.append((key, value))
+        
+        
 
 
     def get(self, key):
 
         """Retrieve the value associated with the given key."""
-        # If the key does not exist, return none
-        if key not in self.data:
-            bucket.append(key, value)
-            # FIX FIX FIX
-        return self.data.get(key, None)
+        
+        # - if key in self.data:
+
+        # use _hash to find the bucket index
+        index = self._hash(key)
+
+        # search the bucket for the key
+        bucket = self.table[index]
+
+        # return the value if found, else return None
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+
+                    # if it exists,
+                return self.data[key]
+        
+            # else, return None
+            else:
+                return self.data.get(key, None)
 
     
         
@@ -78,24 +89,33 @@ class HashTable:
     def remove(self, key):
 
         """Remove the key-value pair associated with the given key."""
-        if key not in self.data:
+        # - if key in self.data:
 
-            raise KeyError(f"Key '{key}' does not exist.")
-        # Only remove the key if it exists
-        # This is to prevent KeyError if the key does not exist
+        # use _hash to find the bucket index
+        index = self._hash(key)
 
-        if key in self.data:
+        # search the bucket for the key
+        bucket = self.table[index]
 
-            del self.data[key]
+        # return the value if found, else return None
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+
+                # if it exists, delete the key-value pair 
+                del self.data[key]
+       
+            # else, return None
+            else:
+                return self.data.get(key, None)
 
 
 
-    # TODO: Write a __str__(self) method to return a string representation for debugging
-    # Loop through buckets and print contents in a readable way    
 
-    def __str__(self):
-        """Return a string representation of the HashMap."""
+    # # Loop through buckets and print contents in a readable way    
+
+    # def __str__(self):
+    #     """Return a string representation of the HashMap."""
         
-        return str(self.data)
+    #     return str(self.data)
 
 
