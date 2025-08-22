4181c412507c6d99464e0ac80086a77f9e5df3d5
jvichn1 <jvichn1@wgu.edu>
2025-08-21 22:10:43 -0400

    HashTable initial done

--- HashTable.py
+++ HashTable.py
@@ -17,8 +17,85 @@ class HashTable:
         # modulo to map a key (like package_id) to a bucket index
         return key % self.size
 
+    # insert method to add a key-value pair to the hash table
+    # - use _hash to find the bucket index
+    # - check if the key already exists in the bucket
+    # - else, append the key-value pair to the bucket
+
+    def insert(self, key, value):
+
+        """Add a key-value pair to the hashmap."""
+        if key in self.data:
+
+            raise KeyError(f"Key '{key}' already exists.")
+
+        self.data[key] = value
+
+        # redundant, please decide!!
+    def insert(self, key, value):
+        """Insert a key-value pair into the hash table."""
+        index = self._hash(key)
+        bucket = self.table[index]
+        # Check if the key already exists in the bucket
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                # If it exists, update the value
+                bucket[i] = (key, value)
+                return
+    # 
+    # 
+    # 
+    # 
+    # TODO: Write a get(self, key) method to retrieve a value by its key
+    # use _hash to find the bucket index
+    # search the bucket for the key
+    # return the value if found, else return None
+    # 
+
+
+    def get(self, key):
+
+        """Retrieve the value associated with the given key."""
+        # If the key does not exist, return none
+        if key not in self.data:
+            bucket.append(key, value)
+            # FIX FIX FIX
+        return self.data.get(key, None)
+
+    
+        
         
 
 
+
+
+    # TODO: Write a remove(self, key) method to remove a key-value pair from the hash table
+    # use _hash to find the bucket index
+    # search the bucket for the key (and delete it if found)
+    # 
+    # 
+
+    def remove(self, key):
+
+        """Remove the key-value pair associated with the given key."""
+        if key not in self.data:
+
+            raise KeyError(f"Key '{key}' does not exist.")
+        # Only remove the key if it exists
+        # This is to prevent KeyError if the key does not exist
+
+        if key in self.data:
+
+            del self.data[key]
+
+
+
+    # TODO: Write a __str__(self) method to return a string representation for debugging
+    # Loop through buckets and print contents in a readable way    
+
+    def __str__(self):
+        """Return a string representation of the HashMap."""
+        
+        return str(self.data)
 
 
