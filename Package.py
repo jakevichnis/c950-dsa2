795f5242bb9e9c7456849ce290dc598f798d90e8
jvichn1 <jvichn1@wgu.edu>
2025-08-21 21:28:13 -0400

    Finished Package Class

--- Package.py
+++ Package.py
@@ -1,6 +1,8 @@
 
 # This is the package class for the project
 class Package:
+
+    # intit method for the parameters of the package class
     def __init__(self, package_id, address, weight, city, zip_code, deadline, status="At Hub"):
         self.id = package_id
         self.address = address
@@ -12,6 +14,21 @@ class Package:
 
         # This will be used to track the delivery time of the package (updated later)
         self.delivery_time = None
+
+
+        # String representation of the package object for easy debugging and display
+        def __str__(self):
+            return f"Package ID: {self.id}, 
+            Address: {self.address}, 
+            Weight: {self.weight}kg, 
+            City: {self.city}, 
+            Zip: {self.zip_code}, 
+            Deadline: {self.deadline}, 
+            Status: {self.status}, 
+            Delivery Time: {self.delivery_time}"
+        
+
+        
        
         
     pass
